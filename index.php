68ecc44612db6ef0050ed8e8dd6265ff9cad5119
Pmsul <151816705+Pmsul@users.noreply.github.com>
2025-04-15 16:05:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <h3>EXEC</h3>
     <a href="?insert=exec">Добавить пользователя</a>
-    <a href="?insert=del">Удалить пользователя</a>
+    <a href="?insert=del">Удалить пользователей</a>
     <?
     $sql = "insert into users (login,password) values ('user1','password1'),('user2','password2'),('user3','password3')";
     require_once 'popos/connection.php';
@@ -26,24 +26,59 @@
     ?>
     <h3>PREPARE</h3>
     <a href="?insert=prepIns">Добавить пользователя</a>
-    <a href="?insert=prepDel">Удалить пользователя</a>
+    <a href="?insert=prepDel">Удалить пользователей</a>
+    <a href="?insert=prepShow">Показать пользователей</a>
     <?
     $users = [['user1', 'password1'], ['user2', 'password2'], ['user3', 'password3'], ['user4', 'password4'], ['user5', 'password5']];
     $sql = "insert into users (login,password) values (?,?)";
     $sql2 = "DELETE FROM users";
+    $sql3 = "SELECT * FROM users";
     require_once 'popos/connection.php';
     if (isset($_GET['insert']) && $_GET['insert'] == 'prepIns') {
         $result = $conn->prepare($sql);
+        $i = 0;
         foreach ($users as $elem) {
             $result->execute($elem);
+            $i++;
         }
 
-
+        echo '<br>' . $i;
     } elseif (isset($_GET['insert']) && $_GET['insert'] == 'prepDel') {
         $result = $conn->prepare($sql2)->execute();
-
+        echo '<br>' . $result;
+    } elseif (isset($_GET['insert']) && $_GET['insert'] == 'prepShow') {
+        $result = $conn->prepare($sql3);
+        $result->execute();
+        $result = $result->fetchAll();
+        echo '<br>';
+        foreach ($result as $elem) {
+            echo "$elem[login] $elem[password] <br>";
+        }
     }
+  
     ?>
-</body>
+    <h3>QUERY</h3>
+    <a href="?insert=queryIns">Добавить пользователя</a>
+    <a href="?insert=queryDel">Удалить пользователей</a>
+    <a href="?insert=queryShow">Показать пользователей</a>
+    <?
+    $sql = "insert into users (login,password) values ('user1','password1'),('user2','password2'),('user3','password3')";
+    $sql2 = "DELETE FROM users";
+    $sql3 = "SELECT * FROM users";
+    require_once 'popos/connection.php';
+    if (isset($_GET['insert']) && $_GET['insert'] == 'queryIns') {
+        $conn->query($sql);
+        echo 'Добавил';
+    } elseif (isset($_GET['insert']) && $_GET['insert'] == 'queryDel') {
+        $conn->query($sql2);
+        echo 'Удалил';
+    } elseif (isset($_GET['insert']) && $_GET['insert'] == 'queryShow') {
+        $result = $conn->query($sql3);
+        $result = $result->fetchAll();
+        echo '<br>';
+        foreach ($result as $elem) {
+            echo "$elem[login] $elem[password] <br>";
+        }
+    }
 
-</html>
+    ?>
